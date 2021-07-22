07c20199a543aae9c326e740d73c68c04dc70228
Muhammad Bilal <bilal.se74@gmail.com>
2021-07-22 09:26:34 +0500

    first upload

---
+++ FPS MULTIPLAYER GAME/Assets/Scripts/GunRotationNetwork.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GunRotationNetwork : MonoBehaviour
+{
+
+    Vector3 realPosition;
+    Quaternion realRotation;
+
+    PhotonView PV;
+
+
+    private void Awake()
+    {
+        PV.GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (PV.IsMine)
+        {
+
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, .1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, .1f);
+        }
+    }
+
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            realPosition = (Vector3)stream.ReceiveNext();
+            realRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
---
+++ FPS MULTIPLAYER GAME/Assets/Scripts/SingleShotGun.cs
@@ -0,0 +1,72 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShotGun : Gun
+{
+	[SerializeField] Camera cam;
+	
+	[SerializeField] GameObject muzzleFlash;
+	[SerializeField] Transform muzzleFlashPosition;
+
+	PhotonView PV;
+
+	[SerializeField] Animator anim;
+
+	void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
+
+    private void Start()
+    {
+		//muzzleFlashPosition = transform.Find("RifleMuzzleFlash");
+    }
+
+    public override void Use()
+	{
+		Shoot();
+	}
+
+	
+	void Shoot()
+	{
+		//anim.SetTrigger("shoot");
+		//anim.SetBool("shoot bool", false);
+
+		//1. use below code if you want to see the muzzle on every player view 
+		//Destroy(PhotonNetwork.Instantiate("muzzleFlash", muzzleFlashPosition.position, Quaternion.identity),
+			//.03f);
+		//PV.RPC("MuzzleFlash", RpcTarget.All);
+		//--------------------------------------------  1.
+
+		Destroy(Instantiate(muzzleFlash, muzzleFlashPosition.position, Quaternion.identity), .03f);
+
+
+
+		Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+		ray.origin = cam.transform.position;
+		if(Physics.Raycast(ray, out RaycastHit hit))
+		{
+			hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+			PV.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
+		}
+	}
+
+	
+
+	[PunRPC]
+	void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
+	{
+		
+
+		Collider[] colliders = Physics.OverlapSphere(hitPosition, 0.3f);
+		if(colliders.Length != 0)
+		{
+			GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hitPosition + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+			Destroy(bulletImpactObj, 10f);
+			bulletImpactObj.transform.SetParent(colliders[0].transform);
+		}
+	}
+}
